426a5c55f78d8a42ef54970f52e74e2b417124b0
Nahuel <nahuelguirao123@gmail.com>
2024-05-14 14:03:39 -0300

    Agrego configuración CORS con whitelist

--- src/app.js
+++ src/app.js
@@ -2,6 +2,7 @@ require('dotenv').config()
 const express = require('express')
 const userRouter = require('./routes/userRoutes')
 const { checkDatabaseConnection } = require('./services/database')
+const cors = require('cors')
 
 //Init app
 const app = express()
@@ -9,7 +10,21 @@ const app = express()
 //Check if conection with Postgre is correct
 checkDatabaseConnection()
 
+//Cors Config
+const whitelist = ['http://localhost:5174']
+
+const corsOptions = {
+    origin: function (origin, callback) {
+        if (whitelist.indexOf(origin) !== -1 || !origin) {
+            callback(null, true);
+        } else {
+            callback(new Error('Not allowed by CORS'));
+        }
+    }
+};
+
 //Middlewares and routers
+app.use(cors(corsOptions));
 app.use(express.json())
 app.use("/users", userRouter)
 
